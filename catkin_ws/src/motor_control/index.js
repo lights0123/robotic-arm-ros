95f01f740f1062545c9e76106136692c9c0ef928
lights0123 <me@lights0123.com>
2018-02-21 22:51:41 -0500

    Start work on web interface

--- catkin_ws/src/motor_control/index.js
+++ catkin_ws/src/motor_control/index.js
@@ -3,9 +3,10 @@ const PivotPi = require('node-pivotpi');
 const motorSettings = {
 	dcs:['M1','M2','M3','M4'],
 	address:0x60
-}
+};
 const motorHat = require('motor-hat')(motorSettings);
 const rosnodejs = require('rosnodejs');
 rosnodejs.initNode('/motor_control').then(()=>{
 	const nh = rosnodejs.nh;
 });
+
